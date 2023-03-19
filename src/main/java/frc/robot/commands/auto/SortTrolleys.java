5c82bd18038599ae2d842dfefcf3024790ae4398
MaxwellGuico <76728084+MaxwellGuico@users.noreply.github.com>
2023-03-19 23:07:58 +0800

    3/19/23 Latest

--- src/main/java/frc/robot/commands/auto/SortTrolleys.java
+++ src/main/java/frc/robot/commands/auto/SortTrolleys.java
@@ -6,12 +6,13 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Globals;
 import frc.robot.RobotContainer;
 import frc.robot.Astar.Layout;
 // The routine to transport the trolleys to the target areas
 public class SortTrolleys extends SequentialCommandGroup{
-   
+    private static Pose2d point = new Pose2d();
     public SortTrolleys(){
         super(
            
@@ -27,7 +28,7 @@ public class SortTrolleys extends SequentialCommandGroup{
             // new TrolleyHolder(0),
             // new MoveRobot(1, -0.05, 0, 0, 0.1),
             // // new InstantCommand(() ->Globals.debug[0]=0),
-            // new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("BlueTarget", Layout.Convert_mm_Pose2d(Layout.BluePos))),
+            // new InstantCommand(()-> RobotContainer.m_points.addObsPoint("BlueTarget", Layout.Convert_mm_Pose2d(Layout.BluePos))),
             // // new InstantCommand(() ->Globals.debug[0]=1),
             // new InstantCommand(() -> RobotContainer.m_points.AddObsGrid()),
             // // new InstantCommand(() ->Globals.debug[0]=2),
@@ -41,7 +42,7 @@ public class SortTrolleys extends SequentialCommandGroup{
             // new GotoColor(Layout.Convert_mm_Pose2d(Layout.GreenPos)),
             // new TrolleyHolder(0),
             // new MoveRobot(1, -0.05, 0, 0, 0.1),
-            // new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("GreenTarget", Layout.Convert_mm_Pose2d(Layout.GreenPos))),
+            // new InstantCommand(()-> RobotContainer.m_points.addObsPoint("GreenTarget", Layout.Convert_mm_Pose2d(Layout.GreenPos))),
             // new InstantCommand(() -> RobotContainer.m_points.AddObsGrid()),
 
             // new GotoTrolley(Layout.Convert_mm_Pose2d(Layout.T3Pos)),
@@ -52,126 +53,97 @@ public class SortTrolleys extends SequentialCommandGroup{
             // new GotoColor(Layout.Convert_mm_Pose2d(Layout.RedPos)),
             // new TrolleyHolder(0),
             // new MoveRobot(1, -0.05, 0, 0, 0.1),
-            // new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("RedTarget", Layout.Convert_mm_Pose2d(Layout.RedPos))),
+            // new InstantCommand(()-> RobotContainer.m_points.addObsPoint("RedTarget", Layout.Convert_mm_Pose2d(Layout.RedPos))),
             // new InstantCommand(() -> RobotContainer.m_points.AddObsGrid())
 
             // SEQUENCE 2 If Pick first // 
-            new GotoTrolley(Layout.Convert_mm_Pose2d(Layout.T3Pos)),
+            new GotoTrolley(Layout.T3Pos),
             new Align2Trolley(),
             new TrolleyHolder(1),
             new InstantCommand(()-> RobotContainer.m_points.removeObs("T3")), 
             
             new InstantCommand(()-> RobotContainer.m_vision.setColor("Red")),
-            new GotoColor(Layout.Convert_mm_Pose2d(Layout.RedPos)),
+            new GotoColor(Layout.RedPos),
             new TrolleyHolder(0),
             new MoveRobot(1, -0.05, 0, 0, 0.1),
-            new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("RedTarget", Layout.Convert_mm_Pose2d(Layout.RedPos))),
+            new InstantCommand(()-> RobotContainer.m_points.addObsPoint("RedTarget", Layout.RedPos)),
             new InstantCommand(() -> RobotContainer.m_points.AddObsGrid()),
 
-            new GotoTrolley(Layout.Convert_mm_Pose2d(Layout.T2Pos)),
+            new GotoTrolley(Layout.T2Pos),
             new Align2Trolley(),
             new TrolleyHolder(1),
             new InstantCommand(()-> RobotContainer.m_points.removeObs("T2")),
             
             new InstantCommand(()-> RobotContainer.m_vision.setColor("Green")),
-            new GotoColor(Layout.Convert_mm_Pose2d(Layout.GreenPos)),
+            new GotoColor(Layout.GreenPos),
             new TrolleyHolder(0),
             new MoveRobot(1, -0.05, 0, 0, 0.1),
-            new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("GreenTarget", Layout.Convert_mm_Pose2d(Layout.GreenPos))),
+            new InstantCommand(()-> RobotContainer.m_points.addObsPoint("GreenTarget", Layout.GreenPos)),
             new InstantCommand(() -> RobotContainer.m_points.AddObsGrid()),
 
-            new GotoTrolley(Layout.Convert_mm_Pose2d(Layout.T1Pos)),
+            new GotoTrolley(Layout.T1Pos),
             new Align2Trolley(),
             new TrolleyHolder(1),
             new InstantCommand(()-> RobotContainer.m_points.removeObs("T1")),
-            
+            new InstantCommand(()-> System.out.println("After removeobs, Before Setting color blue")),
             new InstantCommand(()-> RobotContainer.m_vision.setColor("Blue")),
-            new GotoColor(Layout.Convert_mm_Pose2d(Layout.BluePos)),
+            new GotoColor(Layout.BluePos),
             new TrolleyHolder(0),
             new MoveRobot(1, -0.05, 0, 0, 0.1),
-            new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("BlueTarget", Layout.Convert_mm_Pose2d(Layout.BluePos))),
+            new InstantCommand(()-> RobotContainer.m_points.addObsPoint("BlueTarget", Layout.BluePos)),
             new InstantCommand(() -> RobotContainer.m_points.AddObsGrid())
         );
     }
-
+    /*
+     * For Task B, pass PointMap
+     */
     public SortTrolleys(Map<String, Pose2d> pointMap){
         super(
-           
-            // new InstantCommand(() ->Globals.debug[0]=-1),
-
-            // SEQUENCE 1 If trolley first // 
-            // new GotoTrolley(Layout.Convert_mm_Pose2d(Layout.T1Pos)),
-            // new Align2Trolley(),
-            // new TrolleyHolder(1),
-            
-            // new InstantCommand(()-> RobotContainer.m_vision.setColor("Blue")),
-            // new GotoColor(Layout.Convert_mm_Pose2d(Layout.BluePos)),
-            // new TrolleyHolder(0),
-            // new MoveRobot(1, -0.05, 0, 0, 0.1),
-            // // new InstantCommand(() ->Globals.debug[0]=0),
-            // new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("BlueTarget", Layout.Convert_mm_Pose2d(Layout.BluePos))),
-            // // new InstantCommand(() ->Globals.debug[0]=1),
-            // new InstantCommand(() -> RobotContainer.m_points.AddObsGrid()),
-            // // new InstantCommand(() ->Globals.debug[0]=2),
-
-            // new GotoTrolley(Layout.Convert_mm_Pose2d(Layout.T2Pos)),
-            // new Align2Trolley(),
-            // // new InstantCommand(() ->Globals.debug[0]=3),
-            // new TrolleyHolder(1),
-            
-            // new InstantCommand(()-> RobotContainer.m_vision.setColor("Green")),
-            // new GotoColor(Layout.Convert_mm_Pose2d(Layout.GreenPos)),
-            // new TrolleyHolder(0),
-            // new MoveRobot(1, -0.05, 0, 0, 0.1),
-            // new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("GreenTarget", Layout.Convert_mm_Pose2d(Layout.GreenPos))),
-            // new InstantCommand(() -> RobotContainer.m_points.AddObsGrid()),
-
-            // new GotoTrolley(Layout.Convert_mm_Pose2d(Layout.T3Pos)),
-            // new Align2Trolley(),
-            // new TrolleyHolder(1),
-            
-            // new InstantCommand(()-> RobotContainer.m_vision.setColor("Red")),
-            // new GotoColor(Layout.Convert_mm_Pose2d(Layout.RedPos)),
-            // new TrolleyHolder(0),
-            // new MoveRobot(1, -0.05, 0, 0, 0.1),
-            // new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("RedTarget", Layout.Convert_mm_Pose2d(Layout.RedPos))),
-            // new InstantCommand(() -> RobotContainer.m_points.AddObsGrid())
-
             // SEQUENCE 2 If Pick first // 
-            new GotoTrolley(pointMap.get("T3")),
+            // new InstantCommand(()-> System.out.println(":::::::::::::::::::")),
+            // new InstantCommand(()-> System.out.println(pointMap.get("T3").getTranslation())),
+            // new InstantCommand(()-> System.out.println(RobotContainer.m_Grid.findGotoPos(pointMap.get("T3").getTranslation(), 0.5))),
+
+            // new WaitCommand(5),
+            // new InstantCommand(()-> System.out.println(":::::::::::::::::::")),
+            // new MovetoB(RobotContainer.m_Grid.findGotoPos(pointMap.get("T3").getTranslation(), 0.5)),
+            // new Rotate2Orientation(pointMap.get("T3").getRotation().getDegrees()),
+            
+            new CheckAndMoveTarget("T3", 0.5),
+            
             new Align2Trolley(),
             new TrolleyHolder(1),
             new InstantCommand(()-> RobotContainer.m_points.removeObs("T3")), 
             
             new InstantCommand(()-> RobotContainer.m_vision.setColor("Red")),
-            new GotoColor(pointMap.get("RedTarget")),
+            new GotoColor("RedTarget"),
             new TrolleyHolder(0),
             new MoveRobot(1, -0.05, 0, 0, 0.1),
-            new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("RedTarget", pointMap.get("RedTarget"))),
+            new InstantCommand(()-> RobotContainer.m_points.addObsPoint("RedTarget", pointMap.get("RedTarget"))),
             new InstantCommand(() -> RobotContainer.m_points.AddObsGrid()),
 
-            new GotoTrolley(pointMap.get("T2")),
+            new CheckAndMoveTarget("T2", 0.5),
             new Align2Trolley(),
             new TrolleyHolder(1),
             new InstantCommand(()-> RobotContainer.m_points.removeObs("T2")),
             
             new InstantCommand(()-> RobotContainer.m_vision.setColor("Green")),
-            new GotoColor(pointMap.get("GreenTarget")),
+            new GotoColor("GreenTarget"),
             new TrolleyHolder(0),
             new MoveRobot(1, -0.05, 0, 0, 0.1),
-            new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("GreenTarget", pointMap.get("GreenTarget"))),
+            new InstantCommand(()-> RobotContainer.m_points.addObsPoint("GreenTarget", pointMap.get("GreenTarget"))),
             new InstantCommand(() -> RobotContainer.m_points.AddObsGrid()),
 
-            new GotoTrolley(pointMap.get("T1")),
+            new CheckAndMoveTarget("T1", 0.5),
             new Align2Trolley(),
             new TrolleyHolder(1),
             new InstantCommand(()-> RobotContainer.m_points.removeObs("T1")),
             
             new InstantCommand(()-> RobotContainer.m_vision.setColor("Blue")),
-            new GotoColor(pointMap.get("BlueTarget")),
+            new GotoColor("BlueTarget"),
             new TrolleyHolder(0),
             new MoveRobot(1, -0.05, 0, 0, 0.1),
-            new InstantCommand(()-> RobotContainer.m_points.updateObsPoint("BlueTarget", pointMap.get("BlueTarget"))),
+            new InstantCommand(()-> RobotContainer.m_points.addObsPoint("BlueTarget", pointMap.get("BlueTarget"))),
             new InstantCommand(() -> RobotContainer.m_points.AddObsGrid())
         );
     }
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,159 +1,172 @@
 package frc.robot.subsystems;
 
+import java.util.Map;
+import java.util.function.Supplier;
+
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Globals;
-import frc.robot.Robot;
+import frc.robot.OmniDriveOdometry;
 import frc.robot.RobotContainer;
-import java.util.Map;
+
+
 public class Vision extends SubsystemBase{
+    
     private final ShuffleboardTab tab = Shuffleboard.getTab("Vision");
     private NetworkTableInstance inst = NetworkTableInstance.getDefault();
-    private NetworkTable visionTable = inst.getTable("Shuffleboard/Vision");
+    private NetworkTable table = inst.getTable("Shuffleboard/Vision");
+    // private final NetworkTableEntry D_cW = tab.add("cW", 0).getEntry();
+    private final NetworkTableEntry D_curTarget = tab.add("curTarget", 0).getEntry();
+    private final NetworkTableEntry D_curBin = tab.add("curBin", 0).getEntry();
+    // private final NetworkTableEntry D_JagabeeCount = tab.add("JagCnt", 0).getEntry();
+    // private final NetworkTableEntry D_DettolCount = tab.add("DettolCnt", 0).getEntry();
+    // private final NetworkTableEntry D_CokeCount = tab.add("CokeCnt", 0).getEntry();
+    private final NetworkTableEntry D_WOB = tab.add("WOBList", 0).getEntry();
+    
+    private final static Arm m_arm = RobotContainer.m_arm;
     private final NetworkTableEntry D_currentItem = tab.add("CurrentItem", 0).getEntry();
     private final NetworkTableEntry D_currentItemX = tab.add("CurrentItemX", 0).getEntry();
     private final NetworkTableEntry D_currentItemY = tab.add("CurrentItemY", 0).getEntry();
-    private final NetworkTableEntry D_cvMode = tab.add("cvMode", -1).getEntry();
+    private final NetworkTableEntry D_AddedArmX = tab.add("AddedArmX", 0).getEntry();
+    private final NetworkTableEntry D_AddedRobotX = tab.add("AddedRobotX", 0).getEntry();
+    private final NetworkTableEntry D_cvMode = tab.add("cvMode", 0).getEntry(); 
     private final NetworkTableEntry D_colorMode = tab.add("ColorMode", 0).getEntry();
-    public final NetworkTableEntry D_targetXArm = tab.add("targetXArm", 0).getEntry();
-
-    private final NetworkTableEntry D_CameraMountOffsetX = tab.addPersistent("CameraMountOffsetX", 0.01).withWidget(BuiltInWidgets.kNumberSlider)
+    // private final NetworkTableEntry D_useTF = tab.add("useTF", 0).getEntry();
+    private final NetworkTableEntry D_CameraMountOffsetX = tab.addPersistent("CameraMountOffsetX", 0.015).withWidget(BuiltInWidgets.kNumberSlider)
     .withProperties(Map.of("min", 0, "max", 0.1)).getEntry();
-    private final NetworkTableEntry D_ConvertPxToM = tab.addPersistent("ConvertPxToM", 0.000625).withWidget(BuiltInWidgets.kNumberSlider)
+    private final NetworkTableEntry D_ConvertPxToM = tab.addPersistent("ConvertPxToM", 0.0006225).withWidget(BuiltInWidgets.kNumberSlider)
     .withProperties(Map.of("min", 0, "max", 0.0007)).getEntry();
     private final NetworkTableEntry D_ArmOffsetZ = tab.addPersistent("ArmOffsetZ", 0.25).withWidget(BuiltInWidgets.kNumberSlider)
             .withProperties(Map.of("min", 0, "max", 0.5)).getEntry();
-    private final NetworkTableEntry D_GripperOffsetZ = tab.addPersistent("GripperOffsetZ", 0.16).withWidget(BuiltInWidgets.kNumberSlider)
+    private final NetworkTableEntry D_GripperOffsetZ = tab.addPersistent("GripperOffsetZ", 0.19).withWidget(BuiltInWidgets.kNumberSlider)
             .withProperties(Map.of("min", 0, "max", 0.25)).getEntry();
-    private final NetworkTableEntry D_CokeRatio = tab.addPersistent("CokeRatio", 0.85).withWidget(BuiltInWidgets.kNumberSlider)
+    private final NetworkTableEntry D_CokeRatio = tab.addPersistent("CokeRatio", 0.79).withWidget(BuiltInWidgets.kNumberSlider)
             .withProperties(Map.of("min", 0, "max", 1)).getEntry();
-
+    // private final NetworkTableEntry D_WOBArray = tab.add("WOB array", 0).withWidget(BuiltInWidgets.kTextView).getEntry();      
     private double[] defaultValue = new double[13];
-
-    private final static Arm m_arm = RobotContainer.m_arm;
-
-
+    
     public Vision(){
 
-        m_arm.setCameraAngle(300); // Look down
+  
     }
-    public double[] getLine(){
+    
+    public double [] getLine(){
 
-        double[] line = (visionTable.getEntry("line").getDoubleArray(defaultValue));
-
+      double[] line = (table.getEntry("line").getDoubleArray(defaultValue));
       return line;
     }
-    public void getWOBItems(){
-        // reads the array passed to the networktable
-        double[] WOB = visionTable.getEntry("WOB").getDoubleArray(defaultValue);
-        // stores the data in Globals in a 2d array
-        int[][] Targets = new int[3][3];
-        int index = 0;
-            for (int ROW = 0; ROW < 3; ROW++){
-              for (int COL = 0; COL < 3; COL++) {
-              Targets[ROW][COL] = (int) WOB[index];
-              index++;
-              }
-            }
-        Globals.Targets = Targets;
-      }
     
+    public double getResolution(int wh){
+      double[] dimension = new double[2];
+      dimension[0] = 800;//(SmartDashboard.getNumber("imW",0));
+      dimension[1] = 600;//(SmartDashboard.getNumber("imH",0));
+      return dimension[wh];
+    }
+
+    // gets the 1d array passed from networktables and stores it in a 2d array
+    public void getWOBItems(){
+      // reads the array passed to the networktable
+      double[] WOB = table.getEntry("WOB").getDoubleArray(defaultValue);
+      // D_WOB.setString(""+ WOB[0] + WOB[1]+ WOB[2]+ "," + WOB[3]+ WOB[4]+ WOB[5]+ "," + WOB[6]+ WOB[7]+ WOB[8]);
+      // stores the data in Globals in a 2d array
+      int[][] Targets = new int[3][3];
+      int index = 0;
+		  for (int ROW = 0; ROW < 3; ROW++){
+		    for (int COL = 0; COL < 3; COL++) {
+		    Targets[ROW][COL] = (int) WOB[index];
+		    index++;
+		    }
+		  }
+      Globals.Targets = Targets;
+    }
 
     public double[] getObjects(){
-        // CokeU, Coke, Dettol, Jagabee
-       
-        double[] objects = (visionTable.getEntry("objects").getDoubleArray(defaultValue));
-        
-        return objects;
-    }
+     /*
+       * 0 - CokeU Count
+       * 1,2 - CokeU X,Y
+       * 3 - Coke Count
+       * 4,5 - Coke X,Y 
+       * 6 - Dettol Count
+       * 7,8 - Dettol X,Y 
+       * 9 - Jagabee Count
+       * 10,11 - Jagabee X,Y
+       */
+     
+      double[] objects = (table.getEntry("objects").getDoubleArray(defaultValue));
+      
+      return objects;
+  }
 
-    // Get Distance of colored target from camera
+  public void setColor(String color){
+      /*
+      * Black : 0
+      * Red : 1
+      * Green : 2
+      * Blue: 3
+      */
+      int mode = 0;
+      if (color == "Black"){
+          mode = 0;
+      }
+      else if(color == "Red"){
+          mode = 1;
+      }
+      else if(color == "Green"){
+          mode = 2 ;
+      }
+      else if(color == "Blue"){
+          mode = 3;
+      }
+      D_colorMode.setNumber(mode);
+      
+  }
 
-    public double[] getDistanceTarget(String targetName){
-        double[] distance = (visionTable.getEntry(targetName).getDoubleArray(defaultValue));
-        return distance;
+  public String array(){
+    String n = "tt";
+    String.format(" %s", n);
+    return n;
+  }
+    public void initialize(){
+      // Code written here will run before execute //
+     Globals.curBin = 0;
+     Globals.curTarget = 0;
+     Globals.curItemType = 0;
+     Globals.curAngle = 0;
+     m_arm.setCameraAngle(286);
+      
     }
-    public void setColor(String color){
-        /*
-         * Black : 0
-         * Red : 1
-         * Green : 2
-         * Blue: 3
-         */
-        int mode = 0;
-        if (color == "Black"){
-            mode = 0;
-        }
-        else if(color == "Red"){
-            mode = 1;
-        }
-        else if(color == "Green"){
-            mode = 2 ;
-        }
-        else if(color == "Blue"){
-            mode = 3;
-        }
-        D_colorMode.setNumber(mode);
-        
-    }
-    
-    // public void updatePoint(String targetName){
-    //     double x, y;
-    //     int w = (int)Globals.curPose.getRotation().getDegrees();
-    //     if (w != -90){
-    //         y = Globals.curPose.getTranslation().getY() + getDistanceTarget(targetName)[1] + RobotContainer.m_points.camOffset.getTranslation().getY();
-    //         x = Globals.curPose.getTranslation().getX() + getDistanceTarget(targetName)[0] + RobotContainer.m_points.camOffset.getTranslation().getX();
-    //     }
-    //     else{
-    //         x = Globals.curPose.getTranslation().getX() + getDistanceTarget(targetName)[1] + RobotContainer.m_points.camOffset.getTranslation().getY();
-    //         y = Globals.curPose.getTranslation().getY() - getDistanceTarget(targetName)[0] + RobotContainer.m_points.camOffset.getTranslation().getX(); 
-    //     }
-    //     RobotContainer.m_points.updatePoint(targetName, new Pose2d(new Translation2d(x, y), Globals.curPose.getRotation()));  
-    // }
-    
-    // public void updateAllPoints(){
-    //     String[] targetAreas = {"RedTarget", "GreenTarget", "BlueTarget", "Trolley"};
-    //     for (String targetName: targetAreas){
-    //         if(getDistanceTarget(targetName)[0] != 0 && getDistanceTarget(targetName)[1] != 0 ){
-    //             updatePoint(targetName);
-    //         }
-    //     }
-    // }
-
+   
     @Override
     public void periodic()
     {
-        Globals.convertPxToM = D_ConvertPxToM.getDouble(0.000625);
-        Globals.camera_mount_offset_x = D_CameraMountOffsetX.getDouble(0.01);
-        Globals.arm_offset_z = D_ArmOffsetZ.getDouble(0.25);
-        Globals.gripper_offset = D_GripperOffsetZ.getDouble(0.16);
-        Globals.CokeRatio = D_CokeRatio.getDouble(0.85);
+      Globals.convertPxToM = D_ConvertPxToM.getDouble(0.0006225);
+      Globals.camera_mount_offset_x = D_CameraMountOffsetX.getDouble(0.015);
+      Globals.arm_offset_z = D_ArmOffsetZ.getDouble(0.25);
+      Globals.gripper_offset = D_GripperOffsetZ.getDouble(0.19);
+      Globals.CokeRatio = D_CokeRatio.getDouble(0.79);
+      SmartDashboard.putString("Points Map", RobotContainer.m_points.pointMap.toString());
+      SmartDashboard.putString("Obs Map", RobotContainer.m_points.obstacleMap.toString());
+        D_curBin.setNumber(Globals.curBin);
+        D_curTarget.setNumber(Globals.curTarget);
         D_currentItem.setNumber(Globals.curItemType);
-        D_currentItemX.setNumber(Globals.curItemX);
-        D_currentItemY.setNumber(Globals.curItemY);
-        /*
-         * Video : -1
-         * Color Detection : 0
-         * Object Detection: 1
-         * WOB Detection : 2
-         * Perspective Transformation : 3
-         * Trolley Detection with Transformation: 4
-         * Trolley Alignment: 5
-         */
+        // D_currentItemX.setNumber(Globals.curItemX);
+        // D_currentItemY.setNumber(Globals.curItemY);
+        // D_JagabeeCount.setNumber(getObjects()[0]);
+        // D_DettolCount.setNumber(getObjects()[3]);
+        // D_CokeCount.setNumber(getObjects()[6]);
+        // D_WOBArray.setString(String.format("R:%s,%s,%s G:%s,%s,%s B:%s,%s,%s",Globals.Targets[0][0],Globals.Targets[0][1],Globals.Targets[0][2],
+                                                                                      // Globals.Targets[1][0],Globals.Targets[1][1],Globals.Targets[1][2],
+                                                                                      // Globals.Targets[2][0],Globals.Targets[2][1],Globals.Targets[2][2]));
         D_cvMode.setNumber(Globals.cvMode);
-        // SmartDashboard.putString("TrolleyPose", RobotContainer.m_points.pointMap.get("Trolley").toString());
-        // SmartDashboard.putString("GreenTargetPose", RobotContainer.m_points.pointMap.get("GreenTarget").toString());
-        // SmartDashboard.putString("TrolleyObs", RobotContainer.m_points.obstacleMap.get("Trolley").toString());
-        // SmartDashboard.putString("ObsMap", RobotContainer.m_points.obstacleMap.toString());
-        // SmartDashboard.putString("CurPose", Globals.curPose.toString());
-        // SmartDashboard.putNumber("LineY", RobotContainer.m_vision.getLine()[1]);
+    
     }
 }
